a89e3894cd17e9451b26c3613f915af09e2c1ea3
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-02-23 12:12:32 -0800

    works

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   // name and store variables before return statement
   const name = 'Jessica';
-  const post = [{
-    {'title': 'Dinosaurs are awesome'},
-    {'author': 'Sealthy Stegosaurus'},
-    {'body': 'Check out this body property!'},
-    {'comments': ['First!', 'Great post', 'Hire this author now!']}
-  }]
+  const post = {
+    title: 'Dinosaurs are awesome',
+    author: 'Sealthy Stegosaurus',
+    body: 'Check out this body property!',
+    comments: ['First!', 'Great post', 'Hire this author now!']
+  }
   
   return (
 
     <div className="App">
     {/* can use variables with curly brackets */}
     <p>{name}</p>
-    <p>{post}</p>
-
+    <p>{post.title}</p>
+    <p>{post.author}</p>
+    <p>{post.body}</p>
+    <p>{post.comments}</p>
     </div>
   );
 }
