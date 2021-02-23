cf4d2003219b09dd0da7f3f2693336aaa53b3563
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-02-23 12:01:53 -0800

    this also doesn't work

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 function App() {
   // name and store variables before return statement
   const name = 'Jessica';
-  const post = {
-    title: 'Dinosaurs are awesome',
-    author: 'Sealthy Stegosaurus',
-    body: 'Check out this body property!',
-    comments: ['First!', 'Great post', 'Hire this author now!']
-  }
+  const post = [{
+    {'title': 'Dinosaurs are awesome'},
+    {'author': 'Sealthy Stegosaurus'},
+    {'body': 'Check out this body property!'},
+    {'comments': ['First!', 'Great post', 'Hire this author now!']}
+  }]
+  
   return (
 
     <div className="App">
